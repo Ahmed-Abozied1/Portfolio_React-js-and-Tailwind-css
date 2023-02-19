7d2a8058f38784da7e61c315d6a31e2d9348afb9
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-02-19 15:21:39 +0200

    add Skills component

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,9 +1,71 @@
-import React, { createElement } from "react";
+import React, { createElement, useState } from "react";
 import { content } from "../../Content";
+import Modal from 'react-modal';
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    maxWidth:"23rem",
+    width:"90%",
+
+  },
+  overlay:{
+    padd:"2rem",
+
+  }
+};
+Modal.setAppElement('#root');
+
 const Skills = () => {
   const { skills } = content;
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [selectSkill,setSelectSkill]=useState(null)
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <section className="min-h-fit bg-bg_light_primary">
+    {/* modal */}
+    <Modal isOpen={modalIsOpen}
+    onRequestClose={closeModal}
+    style={customStyles}
+    >
+      <div className="flex items-center gap-2">
+
+        <img className="h-10" src={selectSkill?.logo} alt="..."/>
+        <h6>{selectSkill?.name}</h6>
+      </div>
+      <br/>
+      <ul className="list-decimal px-4 font-Poppins sm:text-sm text-xs !leading-7">
+        <li>
+          Lorem ahmed jdsfnsdisdjfbs
+        </li>
+        <li>
+          Lorem ahmed jdsfnsdisdjfbs
+        </li>
+        <li>
+          Lorem ahmed jdsfnsdisdjfbsjgvgjvgjvcjgvckcco
+        </li>
+        <li>
+          Lorem ahmed jdsfnsdisdjfbsojhojuhuohuhuhuhhuhuh
+        </li>
+        <li>
+          Lorem ahmed jdsfnsdisdjfbs
+        </li>
+      </ul>
+      <br/>
+      <div className="flex justify-end">
+        <button onClick={closeModal } className="btn">Close</button>
+      </div>
+    </Modal>
       <div className="md:container px-5 py-14">
         <h2 className="md:text-3xl text-2xl">{skills.title}</h2>
         <h4 className="text-gray">{skills.subtitle}</h4>
@@ -24,7 +86,10 @@ const Skills = () => {
               <div>
                 <h6>{skill.name} </h6>
                 <p className="italic">{skill.para}</p>
-                <div className="text-xl absolute top-3 right-3">
+                <div onClick={()=>{
+                  setSelectSkill(skill)
+                  openModal();
+                }} className="text-xl absolute top-3 right-3">
                   {createElement(skills.icon)}
                 </div>
               </div>
